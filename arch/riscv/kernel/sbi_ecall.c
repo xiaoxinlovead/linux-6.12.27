fa6d2eafb667b3de927fd21fc4422d217d6873d3
YangXin <740132613@qq.com>
2026-06-11 18:02:35 +0800

    fix: separate a6 assignment from declaration to prevent compiler skipping re-initialization

--- arch/riscv/kernel/sbi_ecall.c
+++ arch/riscv/kernel/sbi_ecall.c
@@ -32,8 +32,9 @@ struct sbiret __sbi_ecall(unsigned long arg0, unsigned long arg1,
 	register uintptr_t a3 asm ("a3") = (uintptr_t)(arg3);
 	register uintptr_t a4 asm ("a4") = (uintptr_t)(arg4);
 	register uintptr_t a5 asm ("a5") = (uintptr_t)(arg5);
-	register uintptr_t a6 asm ("a6") = (uintptr_t)(fid);
+	register uintptr_t a6 asm ("a6");
 	register uintptr_t a7 asm ("a7") = (uintptr_t)(ext);
+	a6 = (uintptr_t)(fid);
 	asm volatile ("ecall"
 		       : "+r" (a0), "+r" (a1), "+r" (a6)
 		       : "r" (a2), "r" (a3), "r" (a4), "r" (a5), "r" (a7)
